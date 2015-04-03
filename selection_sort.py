8e63f1bb482a752d741947642e9dab5046bc2a7b
rayray2002 <rayray2002.huang@gmail.com>
2015-04-03 15:33:20 +0800

    ppm

--- selection_sort.py
+++ selection_sort.py
@@ -1,15 +1,18 @@
 list = [1, 45, 6, 38, 2, 8, 29, 60, 18, 35]
-#answer[1, 2, 6, 8, 18, 29, 35, 38, 45, 60]
+answer = [1, 2, 6, 8, 18, 29, 35, 38, 45, 60]
 
-for i in range(len(list)):
-	least = i
-	for j in range(i + 1, len(list)):
-		if list[j] < list[least]:
-			least = j
-			
-	temp = list[least]
-	list[least] = list[i]
-	list[i] = temp
-	
-for k in range(len(list)):
-	print list[k]
+def selection_sort(data):
+	for i in range(len(data)):
+		least = i
+		for j in range(i + 1, len(data)):
+			if data[j] < data[least]:
+				least = j
+				
+		temp = data[least]
+		data[least] = data[i]
+		data[i] = temp
+
+	return data
+
+
+assert(selection_sort(list) == answer)

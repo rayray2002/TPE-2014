c53ca75368fd9fa5bcfb43e8c3dab4e27af574d9
rayray2002 <rayray2002.huang@gmail.com>
2015-08-17 00:57:58 +0800

    Signed-off-by: rayray2002 <rayray2002.huang@gmail.com>

--- edison/laser.py
+++ edison/laser.py
@@ -25,7 +25,7 @@ def my_app(environ, start_response):
 		
 		laser_pin = mraa.Gpio(8)
 		laser_pin.dir(mraa.DIR_OUT)
-		delay = 1
+		delay = 1.0
 		for i in range(100):
 			print '='*10, i
 			print "start bit"

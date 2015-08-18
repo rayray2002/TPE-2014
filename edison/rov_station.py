6ae93f1dff8a75238e7df4f636ea2338ec1f29f2
rayray2002 <rayray2002.huang@gmail.com>
2015-08-18 23:10:13 +0800

    Signed-off-by: rayray2002 <rayray2002.huang@gmail.com>

--- edison/rov_station.py
+++ edison/rov_station.py
@@ -1,6 +1,6 @@
 from wsgiref.simple_server import make_server
 from cgi import parse_qs
-import mraa
+#import mraa
 import time
 from uart import SerialManager
 
@@ -22,20 +22,19 @@ def my_app(environ, start_response):
 	if p.find("/cord")>=0:
 		d = parse_qs(environ['QUERY_STRING'])
 		
-		x = int(d.get('x',[0])[0])
-		y = int(d.get('y',[0])[0])
-		data = [x, y]
-		print x, y
-		
-		laser_pin = mraa.Gpio(8)
-		laser_pin.dir(mraa.DIR_OUT)
+		state = d.get('state',[0])[0]
+		print state
+		smgr.write(state)
+		"""
+		#laser_pin = mraa.Gpio(8)
+		#laser_pin.dir(mraa.DIR_OUT)
 		delay = 0.3 #0.3 * 17 + 2
 		for i in range(5):
 			print '='*10, i
 			print "start bit"
-			laser_pin.write(1)
+			#laser_pin.write(1)
 			time.sleep(delay/3)
-			laser_pin.write(0)
+			#laser_pin.write(0)
 			time.sleep(delay*2/3)
 			
 			for l in range(len(data)):
@@ -45,16 +44,16 @@ def my_app(environ, start_response):
 				for i in range(8):
 					if getbit(data[l],i) == 1:
 						print i, "on"
-						laser_pin.write(1)
+						#laser_pin.write(1)
 					else:
 						print i, "off"
-						laser_pin.write(0)
+						#laser_pin.write(0)
 					time.sleep(delay/3)
-					laser_pin.write(0)
+					#laser_pin.write(0)
 					time.sleep(delay*2/3)
 					
 			time.sleep(2)
-		
+			"""
 	return index()
 
 httpd = make_server('', 8000, my_app)

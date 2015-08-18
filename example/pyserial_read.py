c3079cf7d16c61bd22f58a7a74959640487c9508
Eric <eric@erichuang.com>
2015-08-18 21:09:48 +0800

    Signed-off-by: Eric <eric@erichuang.com>

---
+++ example/pyserial_read.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import serial, time
+
+#initialization and open the port
+#possible timeout values:
+#    1. None: wait forever, block call
+#    2. 0: non-blocking mode, return immediately
+#    3. x, x is bigger than 0, float allowed, timeout block call
+ser = serial.Serial()
+ser.port = "/dev/ttyS2"
+ser.baudrate = 9600
+ser.bytesize = serial.EIGHTBITS #number of bits per bytes
+ser.parity = serial.PARITY_NONE #set parity check: no parity
+ser.stopbits = serial.STOPBITS_ONE #number of stop bits
+#ser.timeout = None          #block read
+ser.timeout = 1            #non-block read
+#ser.timeout = 2              #timeout block read
+ser.xonxoff = False     #disable software flow control
+ser.rtscts = False     #disable hardware (RTS/CTS) flow control
+ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
+ser.writeTimeout = 2     #timeout for write
+
+try: 
+    ser.open()
+except Exception, e:
+    print "error open serial port: " + str(e)
+    exit()
+
+if ser.isOpen():
+    try:
+        ser.flushInput() #flush input buffer, discarding all its contents
+        ser.flushOutput()#flush output buffer, aborting current output 
+
+        numOfLines = 0
+        while True:
+            response = ser.readline()
+            print("read data: " + response)
+            numOfLines = numOfLines + 1
+            if (numOfLines >= 5):
+                break
+        ser.close()
+    except Exception, e1:
+
+    print "error communicating...: " + str(e1)
+else:
+    print "cannot open serial port "
